f7df19abaecc499b2b36dd633fe8a5d7af592e2c
Zoltan <zdebre@gmail.com>
2014-12-29 22:38:09 +1300

    Scaffoldin project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name loyaltyAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the loyaltyAppApp
+ */
+angular.module('loyaltyAppApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name loyaltyAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the loyaltyAppApp
+ */
+angular.module('loyaltyAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
