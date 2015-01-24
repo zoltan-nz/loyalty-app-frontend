007936f9ca403141bff3468ed54e8ee2b70be0bf
Zoltan <zdebre@gmail.com>
2015-01-25 04:58:36 +1300

    Review form validation added

--- app/scripts/controllers/review.js
+++ app/scripts/controllers/review.js
@@ -12,6 +12,8 @@ angular.module('gemStore')
     this.review = {};
 
     this.addReview = function(product) {
+      this.review.createdOn = Date.now();
       product.reviews.push(this.review);
+      this.review = {};
     };
   });
